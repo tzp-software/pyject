c43efcbcd1c123c5cfc09fb8e6619b687e80d24f
Kyle Roux <jstacoder@gmail.com>
2013-10-01 04:36:08 -0700

    fixed merge errors

--- pyject/make_dir.py
+++ pyject/make_dir.py
@@ -1,8 +1,24 @@
 '''
 	pyject.make_dir.py
 '''
+from root import get_env_root
+import os
+
 
 def add_package(projectName,packageName):
-	print 'Adding package {} to project {}'.format(packageName,projectName)
+	#print 'Adding package {} to project {}'.format(packageName,projectName)
+    ROOT_DIR = get_env_root()
+    if not os.path.exists(ROOT_DIR):
+        os.makedirs(ROOT_DIR)
+    os.chdir(ROOT_DIR)
 
-
+    if not projectName in os.listdir(ROOT_DIR):
+        print '{} does not exist!'.format(projectName)
+    if os.path.exists(os.curdir + os.sep + projectName + os.sep + packageName):
+        print '{} exists! cannot overwrite with pyject! must do it by hand!'.format(packageName)
+    dir = projectName + os.sep + projectName + os.sep + packageName
+    
+    if os.makedirs(dir):
+        print 'Made {}'.format(dir)
+    else:
+        print 'Error making {}'.format(dir)
