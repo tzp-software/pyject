63441781563e8dcf7b72998222758709324a8f77
Kyle Roux <jstacoder@gmail.com>
2013-10-01 11:33:33 +0000

    fixed some files

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ except ImportError:
 
 config = {
 	'name': 'pyJect',
-	'version': '1.0.7',
+	'version': '1.1.0',
 	'author': 'Kyle Roux',
 	'author_email': 'jstacoder@gmail.com',
-	'description': 'a command line to to make and orginize projects',
+	'description': 'a command line tool to make and orginize projects',
 	'long_description': open('README','r').read(),
 	'packages': ['pyject'],
+    'py_modules': ['pyject'],
 	'entry_points': {
 		'console_scripts': [
 			'pyject = pyject.main:main'
